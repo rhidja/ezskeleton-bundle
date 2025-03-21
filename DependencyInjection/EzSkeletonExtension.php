4acea6b01e3551fe7955c07c0052fb56bf1e708c
Ramtane Hidja <ramtane.hidja@smile.fr>
2025-03-21 17:51:07 +0100

    Upgrade to Ibexa 4.6

--- DependencyInjection/EzSkeletonExtension.php
+++ DependencyInjection/EzSkeletonExtension.php
@@ -3,27 +3,21 @@ declare(strict_types=1);
 
 namespace Kabylia\EzSkeletonBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-/**
- * This is the class that loads and manages your bundle configuration.
- *
- * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
- */
 class EzSkeletonExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
+    public const EXTENSION_NAME = 'ibexa_elasticsearch';
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
 }
